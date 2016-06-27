1316327b3c1e0beeaa92da22c3f64edcaf4fa912
wwwwenxin <wx@admindeimac-3.local>
2016-06-27 17:49:54 +0800

    Initial Commit

---
+++ wx's calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  wx's calculator
+//
+//  Created by wx on 16/6/27.
+//  Copyright © 2016年 wx's calculator. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
